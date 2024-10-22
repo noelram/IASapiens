fde797312d2cb7ec1bf29a683910d31bbd06d940
noelram <noeleramirez@yahoo.com>
2024-10-22 14:31:24 +0000

    Inicial

---
+++ scripts/Variables.ps1
@@ -0,0 +1,15 @@
+# Default environment file to be read by scripts
+
+# Default values
+$varCompletionModelOpenAI = "gpt-4o"
+$varCompletionModelAzureOpenAI = "gpt-4o"
+$varEmbeddingModel = "text-embedding-ada-002"
+$varASPNetCore = "Development"
+$varInstance = "https://login.microsoftonline.com"
+
+# Constants
+$varAzureOpenAI = "AzureOpenAI"
+$varOpenAI = "OpenAI"
+$varAzureAd = "AzureAd"
+$varNone = "None"
+$varScopes = "access_as_user"
